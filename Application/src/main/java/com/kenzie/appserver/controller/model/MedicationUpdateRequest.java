ce862091feb6f954b51eb15d6ff747175e078501
emilys95 <emily.siesel@mykenzie.snhu.edu>
2023-12-17 16:32:16 -0500

    Added endpoints, findByName() method and wrote updateRequest class

---
+++ Application/src/main/java/com/kenzie/appserver/controller/model/MedicationUpdateRequest.java
@@ -0,0 +1,74 @@
+package com.kenzie.appserver.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+public class MedicationUpdateRequest {
+    @NotEmpty
+    @JsonProperty("name")
+    private String name;
+    @NotEmpty
+    @JsonProperty("id")
+    private String id;
+    @NotEmpty
+    @JsonProperty("timeOfDay")
+    private String timeOfDay;
+    @NotEmpty
+    @JsonProperty("dosage")
+    private String dosage;
+    @NotEmpty
+    @JsonProperty("alertTime")
+    private String alertTime;
+    @JsonProperty("alertDays")
+    private List<String> alertDays;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(String timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getAlertTime() {
+        return alertTime;
+    }
+
+    public void setAlertTime(String alertTime) {
+        this.alertTime = alertTime;
+    }
+
+    public List<String> getAlertDays() {
+        return alertDays;
+    }
+
+    public void setAlertDays(List<String> alertDays) {
+        this.alertDays = alertDays;
+    }
+}
--- Application/src/main/java/com/kenzie/appserver/service/MedicationService.java
+++ Application/src/main/java/com/kenzie/appserver/service/MedicationService.java
@@ -19,6 +19,18 @@ public class MedicationService {
         this.medicationRepository = medicationRepository;
     }
 
+    public List<Medication> findByName(String medicationName) {
+        List<Medication> deletedMedication = new ArrayList<>();
+
+        List<Medication> allMedication = this.getAllMedications();
+        for (Medication medication : allMedication) {
+            if (medication.getName().equals(medicationName)) {
+                deletedMedication.add(medication);
+            }
+        }
+        return deletedMedication;
+    }
+
     public Medication findById(String id) {
 
         return medicationRepository
