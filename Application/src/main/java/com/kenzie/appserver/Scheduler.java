33fb9641e53fe9563a3f8feebcbd3143771f3f87
josephallheart <jrrese01@gmail.com>
2023-12-19 20:50:43 -0500

    Changed 'Id' from rangeKey to attribute

--- Application/src/main/java/com/kenzie/appserver/Scheduler.java
+++ Application/src/main/java/com/kenzie/appserver/Scheduler.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Scheduler {
 
-    // Create any asynchronous services
-
+//    // Create any asynchronous services
+//
 //    @Autowired
 //    private AsynchronousService checkAsyncService;
 //
+
 //    @Scheduled(fixedDelay = 200)
 //    public void schedule() {
 //        checkAsyncService.executeAsynchronously();
