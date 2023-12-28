12ba7b478829370dfe61be05569897a6c1cf6cf9
Julia Wiecek <julia.wiecek@mykenzie.snhu.edu>
2023-12-28 16:52:43 -0600

    in progress get all medication

--- Frontend/src/pages/medicationPage.js
+++ Frontend/src/pages/medicationPage.js
@@ -9,7 +9,7 @@ class MedicationPage extends BaseClass {
 
     constructor() {
         super();
-        this.bindClassMethods(['onCreate'], this);
+        this.bindClassMethods(['onCreate', 'renderTodaysMedication'], this);
         this.dataStore = new DataStore();
     }
 
@@ -21,24 +21,67 @@ class MedicationPage extends BaseClass {
         document.getElementById('FormId').addEventListener('submit', this.onCreate);
         this.client = new MedicationClient();
 
-       /* this.dataStore.addChangeListener(this.renderExample)*/
+        await this.getAllMedication();
+
+       this.dataStore.addChangeListener(this.renderTodaysMedication);
     }
 
     // Render Methods --------------------------------------------------------------------------------------------------
 
-    async renderExample() {
-        let resultArea = document.getElementById("result-info");
+    async renderTodaysMedication() {
+        //let resultArea = document.getElementById("result-info");
 
-        const example = this.dataStore.get("example");
+        //final DayOfWeek dayOfWeek = LocalDateTime.now().getDayOfWeek();
 
-        if (example) {
-            resultArea.innerHTML = `
-                <div>ID: ${example.id}</div>
-                <div>Name: ${example.name}</div>
-            `
-        } else {
-            resultArea.innerHTML = "No Item";
-        }
+        //const day = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US);
+
+//        var selectElement = document.getElementById('choices-multiple-remove-button');
+//        var alertDays = [];
+//
+//        for (var i = 0; i < selectElement.options.length; i++) {
+//            if (selectElement.options[i].selected) {
+//                alertDays.push(selectElement.options[i].value);
+//            }
+//        }
+
+        const allMedications = this.dataStore.get("allMedications");
+        console.log("allMedications:", allMedications);
+            if (Array.isArray(allMedications)) {
+            for (const medication of allMedications) {
+                var contador = 0,
+                select_opt = 0;
+
+                var class_li = ['list_morning list_dsp_none', 'list_evening list_dsp_none', 'list_afternoon list_dsp_none'];
+
+                var cont = '<div class="col_md_1_list">    <p>' + medication.timeOfDay + '</p>    </div> <div class="col_md_2_list"> <h4>' + medication.name + '</h4> <p>' + medication.dosage + '</p> </div>    <div class="col_md_3_list"> <div class="cont_text_date"> <p>' + medication.alertTime + '</p> <p>' + medication.alertDays + '</p> </div>  <div class="cont_btns_options">    <ul>  <li><a href="#finish" onclick="finish_action(' + select_opt + ',' + contador + ');" ><i class="material-icons">&#xE5CA;</i></a></li>   </ul>  </div>    </div>';
+
+                var li = document.createElement('li')
+                li.className = class_li[select_opt] + " li_num_" + contador;
+
+                li.innerHTML = cont;
+                document.querySelectorAll('.cont_princ_lists > ul')[0].appendChild(li);
+
+                setTimeout(function() {
+                    document.querySelector('.li_num_' + contador).style.display = "block";
+                }, 100);
+
+                setTimeout(function() {
+                    document.querySelector('.li_num_' + contador).className = "list_dsp_true " + class_li[select_opt] + " li_num_" + contador;
+                    contador++;
+                }, 200);
+
+//                const separateDiv = document.getElementById('listPage');
+//
+//                var lis = document.createElement('li')
+//
+//                var cont2 = '<div class="col_md_1_list">    <p>' + medication.timeOfDay + '</p>    </div> <div class="col_md_2_list"> <h4>' + medication.name + '</h4> <p>' + medication.dosage + '</p> </div>    <div class="col_md_3_list"> <div class="cont_text_date"> <p>' + medication.alertTime + '</p> <p>' + medication.alertDays + '</p> </div>  <div class="cont_btns_options">    <ul>  <li><a href="#" id="test-link" onclick="" ><i class="material-icons">&#xe5cd;</i></a></li>   </ul>  </div>    </div>';
+//                        // Update the content of the div
+//                        lis.className = class_li[select_opt] + " li_num_" + contador;
+//
+//                                        lis.innerHTML = cont2;
+//                                        separateDiv.appendChild(lis);
+            }
+          }
     }
 
     // Event Handlers --------------------------------------------------------------------------------------------------
@@ -57,6 +100,12 @@ class MedicationPage extends BaseClass {
         } else {
             this.errorHandler("Error doing GET!  Try again...");
         }
+    }
+
+    async getAllMedication() {
+        const allMedications = await this.client.getMedicationList()
+
+        this.dataStore.set("allMedications", allMedications);
     }
 
     async onCreate(event) {
