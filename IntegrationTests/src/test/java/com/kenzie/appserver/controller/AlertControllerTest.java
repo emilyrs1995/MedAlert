0ba47b09cbadefffee5eb8fd077b78404fc77f68
emilys95 <emily.siesel@mykenzie.snhu.edu>
2024-01-07 21:56:42 -0500

    Wrote tests for the alertController

---
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/AlertControllerTest.java
@@ -0,0 +1,94 @@
+package com.kenzie.appserver.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kenzie.appserver.IntegrationTest;
+import com.kenzie.appserver.service.AlertService;
+import com.kenzie.appserver.service.model.Alert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@IntegrationTest
+public class AlertControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private AlertService alertService;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void checkAlertStatus_withReadyAlert_returnsOK() throws Exception {
+        // GIVEN
+        String name = "Aspirin";
+        String Id = UUID.randomUUID().toString();
+        String dosage = "1 pill";
+        String alertTime = LocalTime.now().toString().substring(0, 5);
+        List<DayOfWeek> alertDays = new ArrayList<>();
+        alertDays.add(DayOfWeek.MONDAY);
+        alertDays.add(DayOfWeek.TUESDAY);
+        alertDays.add(DayOfWeek.WEDNESDAY);
+        alertDays.add(DayOfWeek.THURSDAY);
+        alertDays.add(DayOfWeek.FRIDAY);
+        alertDays.add(DayOfWeek.SATURDAY);
+        alertDays.add(DayOfWeek.SUNDAY);
+        Alert alert = new Alert(name, Id, dosage, alertTime, alertDays);
+        // persisted Alert
+        alertService.addAlert(alert);
+
+        // WHEN
+        String response = mvc.perform(get("/alert/alertStatus")
+                    .accept(MediaType.APPLICATION_JSON))
+        // THEN
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        List<String> responseList = mapper.readValue(response, new TypeReference<List<String>>() {});
+        Assertions.assertEquals(1, responseList.size());
+        Assertions.assertTrue(responseList.get(0).contains(name));
+        Assertions.assertTrue(responseList.get(0).contains(dosage));
+    }
+
+    @Test
+    public void checkAlertStatus_withUnreadyAlert_returnsNoContent() throws Exception {
+        // GIVEN
+        String name = "Tylenol";
+        String Id = UUID.randomUUID().toString();
+        String dosage = "2 capsules";
+        String alertTime = LocalTime.now().plusMinutes(15).toString().substring(0, 5);
+        List<DayOfWeek> alertDays = new ArrayList<>();
+        alertDays.add(DayOfWeek.MONDAY);
+        alertDays.add(DayOfWeek.TUESDAY);
+        alertDays.add(DayOfWeek.WEDNESDAY);
+        alertDays.add(DayOfWeek.THURSDAY);
+        alertDays.add(DayOfWeek.FRIDAY);
+        alertDays.add(DayOfWeek.SATURDAY);
+        alertDays.add(DayOfWeek.SUNDAY);
+        Alert alert = new Alert(name, Id, dosage, alertTime, alertDays);
+        // persisted Alert
+        alertService.addAlert(alert);
+
+        // WHEN
+        String response = mvc.perform(get("/alert/alertStatus")
+                        .accept(MediaType.APPLICATION_JSON))
+        // THEN
+                .andExpect(status().isNoContent())
+                .andReturn().getResponse().getContentAsString();
+
+        Assertions.assertTrue(response.isEmpty());
+    }
+}
